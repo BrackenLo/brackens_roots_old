481eeea612f9fc666c341ae51f1797f604588f80
BrackenLo <110333213+BrackenLo@users.noreply.github.com>
2023-05-04 22:22:27 +0100

    Prepping to move into brackens_engine
    I think most of the main work is done for the rendering end of the renderer. I'm going to start the more logistical side of the engine now.

--- brackens_renderer/src/models/model_renderer.rs
+++ brackens_renderer/src/models/model_renderer.rs
@@ -1,7 +1,9 @@
 //===============================================================
 
+use wgpu::util::DeviceExt;
+
 use crate::{
-    pipelines::{instance_pipeline::RawInstancePipeline, Vertex},
+    pipelines::{PipelineBuilderDescriptor, RawPipeline, Vertex},
     render_tools::RenderPassTools,
     textures::{LoadedTexture, Texture},
     Size,
@@ -53,14 +55,14 @@ impl Vertex for RawMeshVertex {
 
 #[repr(C)]
 #[derive(Clone, Copy, bytemuck::Pod, bytemuck::Zeroable)]
-pub struct RawModelInstance {
+pub struct RawMeshInstance {
     pub transform: [f32; 16],
     // Bones?
 }
-impl Vertex for RawModelInstance {
+impl Vertex for RawMeshInstance {
     fn buffer_layout<'a>() -> wgpu::VertexBufferLayout<'a> {
         wgpu::VertexBufferLayout {
-            array_stride: std::mem::size_of::<RawModelInstance>() as wgpu::BufferAddress,
+            array_stride: std::mem::size_of::<RawMeshInstance>() as wgpu::BufferAddress,
             step_mode: wgpu::VertexStepMode::Instance,
             attributes: &[
                 wgpu::VertexAttribute {
@@ -91,7 +93,7 @@ impl Vertex for RawModelInstance {
 //===============================================================
 
 pub struct ModelRenderer {
-    pipeline: RawInstancePipeline,
+    pipeline: RawPipeline,
 
     projection_bind_group: wgpu::BindGroup,
     projection_uniform_buffer: wgpu::Buffer,
@@ -101,8 +103,129 @@ pub struct ModelRenderer {
     depth_texture: Texture,
 }
 impl ModelRenderer {
-    pub fn new(device: &wgpu::Device, format: wgpu::TextureFormat) -> Self {
-        todo!()
+    pub fn new(device: &wgpu::Device, config: wgpu::SurfaceConfiguration) -> Self {
+        //----------------------------------------------
+
+        let projection_matrix = glam::Mat4::orthographic_rh(0., 640., 0., 360., 0., 100.);
+        let projection_uniform_buffer =
+            device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Model Renderer Projection Uniform Buffer"),
+                contents: bytemuck::cast_slice(&projection_matrix.to_cols_array()),
+                usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            });
+
+        let projection_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: Some("Model Renderer Projection Bind Group Layout"),
+                entries: &[wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::VERTEX,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                }],
+            });
+
+        let projection_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("Model Renderer Bind Group"),
+            layout: &projection_bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::Buffer(
+                    projection_uniform_buffer.as_entire_buffer_binding(),
+                ),
+            }],
+        });
+
+        //----------------------------------------------
+
+        let material_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: Some("Model Renderer Bind Group Layout"),
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                            view_dimension: wgpu::TextureViewDimension::D2,
+                            multisampled: false,
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                        count: None,
+                    },
+                ],
+            });
+
+        //----------------------------------------------
+
+        let size = Size {
+            width: config.width,
+            height: config.height,
+        };
+
+        let depth_texture = Texture::create_depth_texture(device, size, "Model Depth Texture");
+
+        //----------------------------------------------
+
+        let builder = PipelineBuilderDescriptor {
+            name: "Model".into(),
+            bind_group_layouts: Some(vec![
+                &projection_bind_group_layout,
+                &material_bind_group_layout,
+            ]),
+            shader: device.create_shader_module(wgpu::ShaderModuleDescriptor {
+                label: Some("Model Renderer Shader"),
+                source: wgpu::ShaderSource::Wgsl(include_str!("model_shader.wgsl").into()),
+            }),
+            primitive: wgpu::PrimitiveState {
+                cull_mode: Some(wgpu::Face::Back),
+                ..Default::default()
+            },
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: Texture::DEPTH_FORMAT,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default(),
+            }),
+            multisample: wgpu::MultisampleState::default(),
+            fragment_targets: vec![Some(wgpu::ColorTargetState {
+                format: config.format,
+                blend: Some(wgpu::BlendState::REPLACE),
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+            multiview: None,
+        };
+
+        let pipeline = RawPipeline::new(
+            device,
+            &[
+                RawMeshVertex::buffer_layout(),
+                RawMeshInstance::buffer_layout(),
+            ],
+            builder,
+        );
+
+        //----------------------------------------------
+
+        Self {
+            pipeline,
+            projection_bind_group,
+            projection_uniform_buffer,
+            material_bind_group_layout,
+            depth_texture,
+        }
+
+        //----------------------------------------------
     }
 
     pub fn resize(&mut self, device: &wgpu::Device, queue: &wgpu::Queue, new_size: Size<u32>) {
@@ -125,6 +248,10 @@ impl ModelRenderer {
         self.depth_texture = Texture::create_depth_texture(device, new_size, "ModelDepthTexture");
     }
 
+    pub fn get_material_layout(&self) -> &wgpu::BindGroupLayout {
+        &self.material_bind_group_layout
+    }
+
     pub fn render(
         &self,
         render_tools: &mut RenderPassTools,
@@ -144,7 +271,22 @@ impl ModelRenderer {
             .start_render_pass(render_tools, Some(depth_stencil_attachment));
 
         render_pass.set_bind_group(0, &self.projection_bind_group);
-        for draw_call in draw_calls {}
+
+        for draw_call in draw_calls {
+            let ModelDrawCall {
+                vertices,
+                indices,
+                index_count,
+                instances,
+                instance_count,
+            } = draw_call.1;
+
+            render_pass.set_bind_group(1, &draw_call.0);
+            render_pass.set_vertex_buffers([vertices, instances], 0);
+            render_pass.set_index_buffer(indices);
+
+            render_pass.draw_index(0..*index_count, 0..*instance_count);
+        }
     }
 }
 
