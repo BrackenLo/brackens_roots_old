49e011e110e1534bb3981fc6046a8137fabcb6d1
BrackenLo <110333213+BrackenLo@users.noreply.github.com>
2023-06-06 20:36:20 +0100

    Better avg fps tracking

--- brackens_tools/src/upkeep.rs
+++ brackens_tools/src/upkeep.rs
@@ -4,6 +4,8 @@ use instant::Instant;
 
 //===============================================================
 
+pub const MAX_FPS_RECORD_SIZE: usize = 6;
+
 pub struct UpkeepTracker {
     elapsed_time: Instant,
     last_frame_instant: Instant,
@@ -13,7 +15,10 @@ pub struct UpkeepTracker {
     frame_count_this_second: u16,
 
     delta: f32,
-    fps: u16,
+
+    fps_list: [u16; MAX_FPS_RECORD_SIZE],
+    fps_instance_counter: usize,
+    fps_sum: f64,
 }
 
 impl Default for UpkeepTracker {
@@ -25,7 +30,10 @@ impl Default for UpkeepTracker {
             second_tracker: 0.,
             frame_count_this_second: 0,
             delta: 0.,
-            fps: 0,
+
+            fps_list: [0; MAX_FPS_RECORD_SIZE],
+            fps_instance_counter: 0,
+            fps_sum: 0.,
         }
     }
 }
@@ -46,7 +54,11 @@ impl UpkeepTracker {
         self.second_tracker += self.delta;
 
         if self.second_tracker > 1. {
-            self.fps = self.frame_count_this_second;
+            self.fps_sum -= self.fps_list[self.fps_instance_counter] as f64;
+            self.fps_sum += self.frame_count_this_second as f64;
+            self.fps_list[self.fps_instance_counter] = self.frame_count_this_second;
+            self.fps_instance_counter = (self.fps_instance_counter + 1) % MAX_FPS_RECORD_SIZE;
+
             self.frame_count_this_second = 0;
             self.second_tracker -= 1.;
         }
@@ -55,10 +67,10 @@ impl UpkeepTracker {
     //----------------------------------------------
 
     pub fn fps(&self) -> u16 {
-        self.fps
+        self.fps_list[self.fps_instance_counter]
     }
     pub fn avg_fps(&self) -> f32 {
-        self.total_frame_count as f32 / self.elapsed_time.elapsed().as_secs_f32()
+        (self.fps_sum / MAX_FPS_RECORD_SIZE as f64) as f32
     }
     pub fn delta(&self) -> f32 {
         self.delta
